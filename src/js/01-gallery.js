b2d2692dd32902e4ba711e843cac459c8dbe0d7c
MariaFandeeva <mfandeeva@gmail.com>
2022-09-29 18:10:02 +0300

    1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryEl = document.querySelector('.gallery');
+const markupEl = createItemsMarkup(galleryItems);
+
+function createItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join('');
+}
+galleryEl.insertAdjacentHTML('afterbegin', markupEl);
+const lightbox = new SimpleLightbox('.gallery a', {});
 
 console.log(galleryItems);
