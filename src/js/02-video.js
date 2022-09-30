0186e7d124565bc82983f569d9b9255a27b8747c
MariaFandeeva <mfandeeva@gmail.com>
2022-09-30 12:17:46 +0300

    2nd done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const throttle = require('lodash.throttle');
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    const time = data.seconds;
+    localStorage.setItem('videoplayer-current-time', time);
+  }, 1000)
+);
+const onTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
