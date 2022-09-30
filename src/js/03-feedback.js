28436e5ae5625f02705c658950ba7b94127135a2
MariaFandeeva <mfandeeva@gmail.com>
2022-09-30 15:12:11 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,89 +1,42 @@
 import throttle from 'lodash.throttle';
 
-const inputEmailEl = document.querySelector('input');
-const inputMessageEl = document.querySelector('textarea');
-const formEl = document.querySelector('form');
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('.email');
+const messageEl = document.querySelector('.message');
 
-formEl.addEventListener('input', throttle(inLocalStoradge, 500));
-formEl.addEventListener('submit', onSubmitForm);
+formEl.addEventListener('input', throttle(inputHandler, 500));
+formEl.addEventListener('submit', submitHandler);
 
 const savedData = localStorage.getItem('feedback-form-state');
-const parsedData = JSON.parse(savedData);
+const parsedata = JSON.parse(savedData);
 
-if (parsedData) {
-  inputEmailEl.value = parsedData.email;
-  inputMessageEl.value = parsedData.message;
+if (parsedata) {
+  emailEl.value = parsedata.email;
+  messageEl.value = parsedata.message;
 }
+// console.log(localStorage);
 
-console.log(localStorage);
-
-function inLocalStoradge() {
-  const email = inputEmailEl.value;
-  const message = inputMessageEl.value;
+function inputHandler() {
+  let email = emailEl.value;
+  let message = messageEl.value;
   localStorage.setItem(
     'feedback-form-state',
     JSON.stringify({ email, message })
   );
 }
 
-function onSubmitForm(event) {
+function submitHandler(event) {
   event.preventDefault();
-  const email = event.currentTarget.elements.email.value;
-  const message = event.currentTarget.elements.message.value;
+  let email = event.currentTarget.elements.email.value;
+  let message = event.currentTarget.elements.message.value;
 
   if (email === '' || message === '') {
-    return alert('Пожалуйста, заполните все поля');
+    return alert('please fill the form');
   }
 
-  const objectData = {
-    message,
-    email,
-  };
-  console.log(objectData);
+  const data = { message, email };
+  console.log(data);
 
   localStorage.removeItem('feedback-form-state');
   formEl.reset();
 }
-
-// import throttle from 'lodash.throttle';
-
-// const formEl = document.querySelector('.feedback-form');
-// const emailEl = document.querySelector('.email');
-// const messageEl = document.querySelector('.message');
-
-// formEl.addEventListener('input', throttle(inputHandler, 500));
-// formEl.addEventListener('submit', submitHandler);
-
-// const savedData = localStorage.getItem('feedback-form-state');
-// const parsedata = JSON.parse(savedData);
-
-// if (parsedata) {
-//   emailEl.value = parsedata.email;
-//   messageEl.value = parsedata.message;
-// }
-// console.log(localStorage);
-
-// function inputHandler() {
-//   let email = emailEl.value;
-//   let message = messageEl.value;
-//   localStorage.setItem(
-//     'feedback-form-state',
-//     JSON.stringify({ email, message })
-//   );
-// }
-
-// function submitHandler(event) {
-//   event.preventDefault();
-//   let email = event.currentTarget.elements.email.value;
-//   let message = event.currentTarget.elements.message.value;
-
-//   if (email === '' || message === '') {
-//     return alert('please fill the form');
-//   }
-
-//   const data = { message, email };
-//   console.log(data);
-
-//   localStorage.removeItem('feedback-form-state');
-//   formEl.reset();
-// }
